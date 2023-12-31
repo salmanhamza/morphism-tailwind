9d55788733bfd4afb83004ac1d0e2b084307965b
Salman Hamza <salmanhamza177@gmail.com>
2023-12-31 18:22:39 +0500

    glassmorphism card added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Tilt from "react-parallax-tilt";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App bg-image">
+      <Tilt className="h-screen flex items-center justify-center">
+        <div className="container h-96 w-96 bg-white bg-opacity-10 rounded-2xl shadow-5xl relative z-2 border border-opacity-30 border-r-0 border-b-0 backdrop-filter backdrop-blur-sm">
+          <form className="h-full flex flex-col justify-evenly items-center">
+            <div className="text-white font-semibold text-2xl tracking-widest">
+              Login form
+            </div>
+            <input
+              type="text"
+              placeholder="username"
+              className="bg-transparent text-xl border border-t-0 border-l-0 border-r-0 focus:outline-none text-white tracking-wide"
+            />
+            <input
+              type="password"
+              placeholder="password"
+              className="bg-transparent  text-xl border border-t-0 border-l-0 border-r-0 focus:outline-none text-white tracking-wide"
+            />
+            <input
+              type="Submit"
+              className="cursor-pointer  rounded-full px-5 py-1 bg-white bg-opacity-30 hover:bg-white hover:bg-opacity-80 "
+            />
+          </form>
+        </div>
+      </Tilt>
+    </div>
+  );
 }
 
-export default App
+export default App;
